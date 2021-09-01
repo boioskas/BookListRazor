fb09e17d9a52d824f9a0f2366c2e92cd4a9a51dc
boioskas <kaiqueboioskass@gmail.com>
2021-09-01 18:02:21 -0300

    Create Book Page Model

--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -14,5 +14,6 @@ namespace BookListRazor.Model
         [Required]
         public String Name { get; set; }
         public String Author { get; set; }
+        public String ISBN { get; set; }
     }
 }
