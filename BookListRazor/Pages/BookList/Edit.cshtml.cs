2a4135cd4c69a4c74b28afac17ea4427a441a675
boioskas <kaiqueboioskass@gmail.com>
2021-09-02 18:14:58 -0300

    Edit Post Handler

--- BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -24,5 +24,20 @@ namespace BookListRazor.Pages.BookList
         {
             Book = await _db.Book.FindAsync(id);
         }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDB = await _db.Book.FindAsync(Book.Id);
+                BookFromDB.Name = Book.Name;
+                BookFromDB.Author = Book.Author;
+                BookFromDB.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
     }
 }
